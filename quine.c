c24cb85c94dce573eb0468365a1289909f94c221
xjhuang <xjhuang@s-an.org>
2017-06-18 16:05:35 +0800

    The code

---
+++ quine.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+char *s = "#include<stdio.h>%cchar *s = %c%s%c;%cint main(){%c%cprintf(s,10,34,s,34,10,10,9,10,9,10,10);%c%creturn 0;%c}%c";
+int main(){
+	printf(s,10,34,s,34,10,10,9,10,9,10,10);
+	return 0;
+}
